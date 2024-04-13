734de64e0ecf7f9574e7160390c1e1553c538226
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-04-13 23:21:47 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our", "your", "my", "her"];
+  let adjective = ["great", "big", "large", "huge"];
+  let noun = ["jogger", "raccoon", "soldier", "gym"];
+  let url = [".com", ".yeah", ".net", ".gov"];
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; ++pronounIndex) {
+    for (let adjIndex = 0; adjIndex < adjective.length; ++adjIndex) {
+      for (let nounIndex = 0; nounIndex < noun.length; ++nounIndex) {
+        for (let urlIndex = 0; urlIndex < url.length; ++urlIndex) {
+          let para = document.createElement("p");
+          para.innerHTML = `${pronoun[pronounIndex]}${adjective[adjIndex]}${noun[nounIndex]}${url[urlIndex]}`;
+          document.getElementById("domain-gen").appendChild(para);
+        }
+      }
+    }
+  }
 };
